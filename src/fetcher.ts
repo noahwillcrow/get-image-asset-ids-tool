1bdddaaa5c0ceb73d7d9e100a6a123dc6108be02
Noah Crowley <noahwillcrow@gmail.com>
2020-07-26 00:31:32 -0400

    get linting working

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -1,18 +1,17 @@
-function getRequest(url: string): Promise<any> {
-    return new Promise<any>(
-      function (resolve, reject) {
-        const request = new XMLHttpRequest();
-        request.onload = function () {
-          if (this.status === 200) {
-            resolve(this.response);
-          } else {
-            reject(new Error(this.statusText));
-          }
-        };
-        request.onerror = function () {
-          reject(new Error('XMLHttpRequest Error: ' + this.statusText));
-        };
-        request.open('GET', url);
-        request.send();
-      });
+function getRequest(url: string): Promise<unknown> {
+	return new Promise<unknown>(function(resolve, reject) {
+		const request = new XMLHttpRequest();
+		request.onload = function() {
+			if (this.status === 200) {
+				resolve(this.response);
+			} else {
+				reject(new Error(this.statusText));
+			}
+		};
+		request.onerror = function() {
+			reject(new Error("XMLHttpRequest Error: " + this.statusText));
+		};
+		request.open("GET", url);
+		request.send();
+	});
 }
